2f46066b2b560ed8e2d27c696f64f26ab19503b7
Chris <chris.davis5440@gmail.com>
2017-07-26 18:23:28 +0000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _ = require("lodash");
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -66,6 +67,33 @@ app.delete("/todos/:id", (req, res) => {
     }
     res.send({todo});
   }, (error) => {
+    res.status(400).send();
+  });
+});
+
+//Update Todo
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+  
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Id is not valid!");
+  }
+  
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    
+    res.send({todo});
+  }).catch((error) => {
     res.status(400).send();
   });
 });
